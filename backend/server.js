d55a5042c5ba98acead3606c32ad8bd53b7fb83f
Mukesh Jaiswal <jaiswalmukesh267@gmail.com>
2026-02-05 01:42:11 +0530

    Fix PORT for production deployment

--- backend/server.js
+++ backend/server.js
@@ -17,10 +17,10 @@ mongoose
 
 // Routes
 app.use("/api/auth", require("./routes/authRoutes"));
-app.use("/api/posts", require("./routes/postRoutes")); // 👈 NEW (STEP-3)
+app.use("/api/posts", require("./routes/postRoutes"));
 
 // Server
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
